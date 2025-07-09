a5b52cb3a0257c2e73dd506464addce4d0ce7a20
Vithartdk01 <vitharshnadhaneshkumar@gmail.com>
2025-07-09 23:40:11 +0530

    Initial Commit

---
+++ player.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.go_to_start()
+        self.right(270)
+
+    def up(self):
+           self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def at_finsh_line(self):
+        if self.ycor()>FINISH_LINE_Y:
+            return True
+        else:
+            return False
